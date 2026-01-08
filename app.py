b8a3d380a79bcac71e5a0d4a0aee7c8fa706e4bc
Sharath Chandra Kumar <54568371+sharath-b@users.noreply.github.com>
2026-01-08 21:40:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-import flask
-print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from ECS container!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
